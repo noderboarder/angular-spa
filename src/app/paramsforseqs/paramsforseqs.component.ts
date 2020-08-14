0ab69c7ca816d66d3b9acca62e2ceeab71dd170a
noderboarder <noderboarder@gmail.com>
2020-08-14 04:38:08 -0500

    default values, reset function, validator

--- src/app/paramsforseqs/paramsforseqs.component.ts
+++ src/app/paramsforseqs/paramsforseqs.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder} from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-paramsforseqs',
@@ -8,23 +8,65 @@ import { FormGroup, FormControl, FormBuilder} from '@angular/forms';
 })
 export class ParamsforseqsComponent implements OnInit {
 
-  seqqueryparameters: FormGroup; // step 3: html의 formgroup name과 동일해야한다.
+  seqqueryparametersForm: FormGroup; // step 3: html의 formgroup name과 동일해야한다.
 
   constructor(private formBuilder: FormBuilder) {
+    /*
       // formBuilder는 form을 빌드하는 역할정도 한다.
-      this.seqqueryparameters = formBuilder.group({
+      this.seqqueryparameters = formBuilder.group(
+        {
+          startingPosition: new FormControl(),
+          endingPosition: new FormControl(),
           maskOptionYn: new FormControl(), // 각각은 elements이며 html의 formControlName에 있는 이름과 같아야함
           maskOption: new FormControl()
         }
       );
+     */
+    // showing message
+    this.seqqueryparametersForm = formBuilder.group({
+        startingPosition: ['', Validators.required], // TODO min, bigger than 0, two number comparing validation
+        endingPosition: ['', Validators.required], // TODO min, bigger than 0, two number comparing validation
+        maskOptionYn: ['', Validators.requiredTrue], // 각각은 elements이며 html의 formControlName에 있는 이름과 같아야함
+        maskOption: ['', Validators.required]
+      }
+    );
+  }
+
+  // tslint:disable-next-line:typedef
+  defaultSet(){
+    // this.seqqueryparametersForm.setValue({
+    this.seqqueryparametersForm.patchValue({ // TODO de-activate by setting checkbox
+      startingPosition: 0,
+      endingPosition: 0
+      // maskOptionYn: true,
+      // maskOption: 'hard'
+    });
   }
 
   ngOnInit(): void {
+    this.defaultSet();
   }
 
+  // tslint:disable-next-line:typedef
   postParamsForSeq() {
-    console.log(this.seqqueryparameters);
-    console.log(this.seqqueryparameters.value);
-    console.log(this.seqqueryparameters.value.maskOptionYn);
+    // test
+    console.log(this.seqqueryparametersForm);
+    // getting the entire values
+    console.log(this.seqqueryparametersForm.value);
+    // getting individual values
+    console.log(this.seqqueryparametersForm.value.startingPosition);
+    console.log(this.seqqueryparametersForm.value.endingPosition);
+    console.log(this.seqqueryparametersForm.value.maskOptionYn);
+
+    this.resetForm();
+    this.seqqueryparametersForm.patchValue({ // TODO de-activate by setting checkbox
+      startingPosition: 0,
+      endingPosition: 0});
+  }
+
+  // tslint:disable-next-line:typedef
+  resetForm() {
+    this.seqqueryparametersForm.reset();
+    this.defaultSet();
   }
 }
